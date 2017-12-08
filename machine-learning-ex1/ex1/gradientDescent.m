f7a20b6da770d36a6c9801fc3e5f9fae37755b50
Greg Attra <greg.attra@gmail.com>
2017-12-07 20:29:59 -0500

    working on ex2

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,8 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-    
     predictions = X * theta;
+    
     J = 0;
     for i = 1:m
       J = J + (predictions(i) - y(i)) * X(i,:)';
