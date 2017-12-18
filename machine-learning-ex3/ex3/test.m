ca0e562955eda2834a1afe4c6e72223d68fbd82c
Greg Attra <greg.attra@gmail.com>
2017-12-18 13:50:57 -0500

    excersize 3

---
+++ machine-learning-ex3/ex3/test.m
@@ -0,0 +1,37 @@
+clear ; close all; clc
+
+%% Setup the parameters you will use for this part of the exercise
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+num_labels = 10;          % 10 labels, from 1 to 10
+                          % (note that we have mapped "0" to label 10)
+
+%% =========== Part 1: Loading and Visualizing Data =============
+%  We start the exercise by first loading and visualizing the dataset.
+%  You will be working with a dataset that contains handwritten digits.
+%
+
+% Load Training Data
+fprintf('Loading and Visualizing Data ...\n')
+
+load('ex3data1.mat'); % training data stored in arrays X, y
+m = size(X, 1);
+
+% Randomly select 100 data points to display
+rand_indices = randperm(m);
+sel = X(rand_indices(1:100), :);
+
+% displayData(sel);
+
+theta_t = [-2; -1; 1; 2];
+X_t = [ones(5,1) reshape(1:15,5,3)/10];
+y_t = ([1;0;1;0;1] >= 0.5);
+lambda_t = 3;
+
+m = length(y_t); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta_t));
+
+predictions = sigmoid(X_t * theta_t);
+sum(-y_t .* log(predictions) - (1 - y_t) .* log(1 - predictions))
